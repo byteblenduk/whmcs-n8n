d8952de3e35e28979fdc03a7741f01c3f44c69c5
ByteBlend <127003436+byteblenduk@users.noreply.github.com>
2024-09-01 03:02:31 +0100

    Created function-tests folder
    For creating function test php files before adapting to whmcs, ensures that the general flow is able to complete fully nwithout the need to test whmcs triggers

---
+++ snippets/function-tests/createaccount.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Function called when creating a new account, including a license key
+** check and 200 status code.
+**
+** Variables usually retrieved from $params will be set towards the end
+** of the file, along with a test $params array
+*/
